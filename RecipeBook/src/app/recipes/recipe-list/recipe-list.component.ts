b628e4b4e6c29aff61b36f914f4809b8d053bbb3
anurag <ADandamudi@beamsoftware.com>
2017-09-06 17:19:24 -0400

    Modify project to look good

--- RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description', 'http://images.indianexpress.com/2015/05/macaroni-main.jpg'),
+    new Recipe('Test Recipe', 'Test Description', 'http://images.indianexpress.com/2015/05/macaroni-main.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
