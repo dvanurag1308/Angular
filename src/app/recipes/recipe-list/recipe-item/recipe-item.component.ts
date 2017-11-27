683ed6d194ae7fa344505832cbff8504af6c8d84
anurag <ADandamudi@beamsoftware.com>
2017-11-26 20:38:00 -0500

    navigation and paths'

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,12 +10,8 @@ export class RecipeItemComponent implements OnInit {
 
   @Input('recipeItem') recipe : Recipe;
 
-  constructor(private recipeService: RecipeService) { }
-
-  recipeClicked() {
-    this.recipeService.selectedRecipe.emit(this.recipe);
-  }
-
+  @Input()id: number
+  
   ngOnInit() {
   }
 
