9daeadd7c3a53d2696ec7c2072feae2c5fbcfefd
dvanurag1308 <dvanurag1308@gmail.com>
2018-01-01 20:25:49 -0500

    Creating seperate modules

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,18 +1,15 @@
-import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from "@angular/core";
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
+    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-    
-
-    constructor(private elementRef: ElementRef, private renderer: Renderer2){}
-    
     @HostBinding('class.open') onClicked = false;
 
-    @HostListener('click') toggleDropdown(eventData: Event){
+    @HostListener('click') toggleDropdown(eventData: Event) {
         this.onClicked = !this.onClicked;
     }
 }
